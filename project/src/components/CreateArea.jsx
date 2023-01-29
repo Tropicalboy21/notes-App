577073d2c8a418c83a124ff2cfc7a09274fef9c1
Lenin Ugalde <93264177+Tropicalboy21@users.noreply.github.com>
2023-01-29 00:03:14 -0600

    Add files via upload

---
+++ project/src/components/CreateArea.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function CreateArea(props) {
+
+    const [note, setNote] = useState(
+        {
+            title: "",
+            content: ""
+        }
+    );
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            };
+        });
+    }
+
+    function submitNote(event) {
+        props.onAdd(note);
+        event.preventDefault();
+        setNote({
+            title: "",
+            content: ""
+        })
+    }
+
+    return (
+        <div>
+            <form>
+                <input name="title" placeholder="Title" value={note.title} onChange={handleChange} />
+                <textarea name="content" placeholder="Take a note..." value={note.content} onChange={handleChange} />
+                <button onClick={submitNote}>Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default CreateArea;
